a7345df3f4997eb2a7cf9b1ba12447a7e95981db
Benn Ma <bennmsg@gmail.com>
2024-04-08 15:00:33 +0800

    fix tests

--- tests/test_app_producer.py
+++ tests/test_app_producer.py
@@ -44,8 +44,21 @@ def client(mocker: MockFixture):
         yield client
 
 
-def test_home(client: TestClient):
-    response = client.get("/")
+def check_event(client: TestClient, request_json, expect_succ=True):
+    response = client.post(
+        "/",
+        json=request_json,
+    )
+    if expect_succ:
+        assert response.status_code == 200
+        assert response.content == b"ok"
+    else:
+        assert response.status_code != 200
+        assert response.content != b"ok"
+
+
+def test_health_check(client: TestClient):
+    response = client.get("/check")
     assert response.status_code == 200
     assert response.content == b"running"
 
@@ -60,7 +73,7 @@ def test_show_config(client: TestClient):
 
 def test_send_an_event(client: TestClient):
     response = client.post(
-        "/main?resp_format=json",
+        "/?resp_format=json",
         json={
             "id": "e1",
             "title": "normal_event",
@@ -71,27 +84,27 @@ def test_send_an_event(client: TestClient):
     assert response.json()["status"] == "all_succ"
 
     response = client.post(
-        "/main?resp_format=json",
+        "/?resp_format=json",
         json={
             "id": "e1",
             "title": "exceptional_event",
             "published": "2021-08-01T18:57:44+02:00",
         },
     )
-    assert response.status_code == 200
+    assert response.status_code != 200
     assert response.json()["status"] == "all_fail"
 
 
 def test_event_format(client: TestClient):
     response = client.post(
-        "/main?resp_format=json",
+        "/?resp_format=json",
         json={"id": "e1"},
     )
     assert response.json()["status"] == "all_fail"
     assert "EventValidationError" in response.json()["details"]["root"]
 
     response = client.post(
-        "/main?resp_format=json",
+        "/?resp_format=json",
         json={
             "id": "e1",
             "title": "normal_event",
@@ -101,12 +114,9 @@ def test_event_format(client: TestClient):
     assert response.json()["status"] == "all_succ"
 
 
-# TODO test published date format
-
-
 def test_resp_format(client: TestClient):
     response = client.post(
-        "/main",
+        "/",
         json={
             "id": "e1",
             "title": "normal_event",
@@ -119,7 +129,7 @@ def test_resp_format(client: TestClient):
 
 def test_send_multiple_events(client: TestClient):
     response = client.post(
-        "/main?resp_format=json",
+        "/?resp_format=json",
         json=[
             {
                 "id": "e1",
@@ -138,7 +148,7 @@ def test_send_multiple_events(client: TestClient):
             },
         ],
     )
-    assert response.status_code == 200
+    assert response.status_code != 200
     assert response.json() == {
         "status": "part_succ",
         "details": {"e2": "<RuntimeError> exceptional_event"},
@@ -147,18 +157,86 @@ def test_send_multiple_events(client: TestClient):
 
 def test_timeout(client: TestClient):
     response = client.post(
-        "/main?resp_format=json&max_resp_time=0.2",
+        "/?resp_format=json&max_resp_time=0.2",
         json={
             "id": "e1",
             "title": "timeout_event",
             "published": "2021-08-01T18:57:44+02:00",
         },
     )
-    assert response.status_code == 200
+    assert response.status_code != 200
     assert response.json() == {
         "status": "all_fail",
         "details": {"e1": "<TimeoutError> "},
     }
 
 
-# TODO test different sort of events
+# === Test different sort of events ===
+def test_non_json_event(client: TestClient):
+    check_event(client, "abc", False)
+
+
+def test_no_id_event(client: TestClient):
+    check_event(client, {"verb": "login"}, False)
+    check_event(client, {"title": "user.login"}, False)
+
+
+def test_normal_event(client: TestClient):
+    check_event(
+        client,
+        {
+            "version": "1.0",
+            "id": "ED-providerId-message.send-actorId-59e704843e9cb",
+            "title": "message.send",
+            "verb": "send",
+            "actor": {"id": "actorId", "objectType": "actorType"},
+            "provider": {"id": "providerId", "objectType": "providerType"},
+            "published": "2017-08-15T13:49:55Z",
+        },
+    )
+
+
+def test_real_event(client: TestClient):
+    check_event(
+        client,
+        {
+            "version": "1.0",
+            "content": '{"a": "testa", "b": "testb"}',
+            "actor": {
+                "id": "actorId",
+                "objectType": "actorType",
+                "content": '{"a": 1, "b": 2}',
+                "attachments": [
+                    {
+                        "id": "attachmentId1",
+                        "objectType": "attachmentType1",
+                        "content": "abc",
+                        "attachments": [
+                            {
+                                "id": "subAttachmentId1",
+                                "objectType": "subAttachmentType1",
+                                "content": "subcontent",
+                            }
+                        ],
+                    },
+                    {
+                        "id": "attachmentId2",
+                        "objectType": "attachmentType2",
+                        "content": "def",
+                    },
+                ],
+            },
+            "object": {"id": "objectId", "objectType": "objectType"},
+            "target": {"id": "targetId", "objectType": "targetType"},
+            "provider": {"id": "providerId", "objectType": "providerType"},
+            "published": "2017-08-15T13:49:55Z",
+            "title": "message.send",
+            "verb": "send",
+            "id": "ED-providerId-message.send-actorId-59e704843e9cb",
+            "generator": {
+                "id": "tnc-event-dispatcher",
+                "objectType": "library",
+                "content": '{"mode":"async","class":"dfEvent_Profile_Visit"}',
+            },
+        },
+    )
